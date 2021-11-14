ae9c556ebcf5402ed569843659de4731c38fc2cd
d-borrego52 <d.borrego@campus.fct.unl.pt>
2021-11-14 09:34:35 +0000

    Storage changes on message retrieves

---
+++ src/main/java/protocols/storage/messages/NoFileInHostMessage.java
@@ -0,0 +1,48 @@
+package protocols.storage.messages;
+
+import io.netty.buffer.ByteBuf;
+import pt.unl.fct.di.novasys.babel.generic.ProtoMessage;
+import pt.unl.fct.di.novasys.network.ISerializer;
+import pt.unl.fct.di.novasys.network.data.Host;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class NoFileInHostMessage extends ProtoMessage {
+
+    public static final short MSG_ID = 235;
+
+    public UUID mid; 
+
+
+    public NoFileInHostMessage(UUID mid) {
+        super(MSG_ID);
+        this.mid = mid;
+       
+    }
+
+    public UUID getUid() {
+        return mid;
+    }
+
+    //TODO - check serializer
+    public static ISerializer<NoFileInHostMessage> serializer = new ISerializer<>() {
+        @Override
+        public void serialize(NoFileInHostMessage msg, ByteBuf out) throws IOException {
+            out.writeLong(msg.mid.getMostSignificantBits());
+            out.writeLong(msg.mid.getLeastSignificantBits());
+        }
+
+        @Override
+        public NoFileInHostMessage deserialize(ByteBuf in) throws IOException {
+            long firstLong = in.readLong();
+            long secondLong = in.readLong();
+            UUID mid = new UUID(firstLong, secondLong);
+
+            //TODO - Check .toString
+            return new NoFileInHostMessage(mid);
+        }
+    };
+}
